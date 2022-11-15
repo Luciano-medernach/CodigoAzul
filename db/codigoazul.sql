97c28d64456d7e204233119ea9ea2c967271d0cb
Luciano Medernach <luciano.medernach@gmail.com>
2022-11-15 18:27:28 -0300

    DB

--- db/codigoazul.sql
+++ db/codigoazul.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 15-11-2022 a las 02:57:16
+-- Tiempo de generación: 15-11-2022 a las 22:27:11
 -- Versión del servidor: 10.4.25-MariaDB
 -- Versión de PHP: 8.1.10
 
@@ -51,7 +51,8 @@ CREATE TABLE `calls` (
   `area` int(11) NOT NULL,
   `origin` int(11) NOT NULL,
   `date` date NOT NULL DEFAULT current_timestamp(),
-  `attended` tinyint(1) NOT NULL DEFAULT 0
+  `attended` tinyint(1) NOT NULL DEFAULT 0,
+  `time` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -76,6 +77,45 @@ INSERT INTO `nurses` (`id`, `name`, `lastname`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Estructura de tabla para la tabla `nurse_area`
+--
+
+CREATE TABLE `nurse_area` (
+  `id` int(11) NOT NULL,
+  `nurseid` int(11) NOT NULL,
+  `areaid` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `nurse_area`
+--
+
+INSERT INTO `nurse_area` (`id`, `nurseid`, `areaid`) VALUES
+(8, 3, 2);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `nurse_patient`
+--
+
+CREATE TABLE `nurse_patient` (
+  `id` int(11) NOT NULL,
+  `nurseid` int(11) NOT NULL,
+  `patientid` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `nurse_patient`
+--
+
+INSERT INTO `nurse_patient` (`id`, `nurseid`, `patientid`) VALUES
+(12, 3, 5),
+(14, 3, 2);
+
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `patients`
 --
 
@@ -83,19 +123,44 @@ CREATE TABLE `patients` (
   `id` int(11) NOT NULL,
   `name` varchar(50) NOT NULL,
   `lastname` varchar(50) NOT NULL,
+  `dni` int(11) NOT NULL,
   `age` int(11) NOT NULL,
+  `address` varchar(50) NOT NULL,
+  `phone` varchar(50) NOT NULL,
+  `family_phone` varchar(50) NOT NULL,
   `height` float NOT NULL,
   `weight` float NOT NULL,
-  `area` int(11) NOT NULL,
-  `nurse` int(11) NOT NULL
+  `medical_history` text DEFAULT NULL,
+  `medicines` text DEFAULT NULL,
+  `area` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Volcado de datos para la tabla `patients`
 --
 
-INSERT INTO `patients` (`id`, `name`, `lastname`, `age`, `height`, `weight`, `area`, `nurse`) VALUES
-(2, 'Julio', 'Martinez', 46, 1.67, 44, 2, 3);
+INSERT INTO `patients` (`id`, `name`, `lastname`, `dni`, `age`, `address`, `phone`, `family_phone`, `height`, `weight`, `medical_history`, `medicines`, `area`) VALUES
+(2, 'Julio', 'Martinez', 0, 46, '', '', '', 1.67, 44, '', '', 2),
+(5, 'Ivanaw', 'Medernach', 174437, 51, '64-156', '2262141510', '2262140631', 1.84, 70, '', '', 2);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `patient_area`
+--
+
+CREATE TABLE `patient_area` (
+  `id` int(11) NOT NULL,
+  `patientid` int(11) NOT NULL,
+  `areaid` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `patient_area`
+--
+
+INSERT INTO `patient_area` (`id`, `patientid`, `areaid`) VALUES
+(14, 2, 2);
 
 -- --------------------------------------------------------
 
@@ -143,12 +208,36 @@ ALTER TABLE `nurses`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indices de la tabla `nurse_area`
+--
+ALTER TABLE `nurse_area`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `nurseid` (`nurseid`),
+  ADD KEY `areaid` (`areaid`);
+
+--
+-- Indices de la tabla `nurse_patient`
+--
+ALTER TABLE `nurse_patient`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `nurseid` (`nurseid`),
+  ADD KEY `patientid` (`patientid`);
+
+--
 -- Indices de la tabla `patients`
 --
 ALTER TABLE `patients`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `nurse` (`nurse`),
+  ADD UNIQUE KEY `dni` (`dni`),
   ADD KEY `area` (`area`) USING BTREE;
+
+--
+-- Indices de la tabla `patient_area`
+--
+ALTER TABLE `patient_area`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `patientid` (`patientid`),
+  ADD KEY `areaid` (`areaid`);
 
 --
 -- Indices de la tabla `users`
@@ -180,16 +269,34 @@ ALTER TABLE `nurses`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
+-- AUTO_INCREMENT de la tabla `nurse_area`
+--
+ALTER TABLE `nurse_area`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT de la tabla `nurse_patient`
+--
+ALTER TABLE `nurse_patient`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+
+--
 -- AUTO_INCREMENT de la tabla `patients`
 --
 ALTER TABLE `patients`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT de la tabla `patient_area`
+--
+ALTER TABLE `patient_area`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
 -- AUTO_INCREMENT de la tabla `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- Restricciones para tablas volcadas
@@ -202,11 +309,31 @@ ALTER TABLE `calls`
   ADD CONSTRAINT `calls_ibfk_1` FOREIGN KEY (`origin`) REFERENCES `areas` (`id`);
 
 --
+-- Filtros para la tabla `nurse_area`
+--
+ALTER TABLE `nurse_area`
+  ADD CONSTRAINT `nurse_area_ibfk_1` FOREIGN KEY (`nurseid`) REFERENCES `nurses` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `nurse_area_ibfk_2` FOREIGN KEY (`areaid`) REFERENCES `areas` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `nurse_patient`
+--
+ALTER TABLE `nurse_patient`
+  ADD CONSTRAINT `nurse_patient_ibfk_1` FOREIGN KEY (`nurseid`) REFERENCES `nurses` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `nurse_patient_ibfk_2` FOREIGN KEY (`patientid`) REFERENCES `patients` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
 -- Filtros para la tabla `patients`
 --
 ALTER TABLE `patients`
-  ADD CONSTRAINT `patients_ibfk_1` FOREIGN KEY (`area`) REFERENCES `areas` (`id`),
-  ADD CONSTRAINT `patients_ibfk_2` FOREIGN KEY (`nurse`) REFERENCES `nurses` (`id`);
+  ADD CONSTRAINT `patients_ibfk_1` FOREIGN KEY (`area`) REFERENCES `areas` (`id`);
+
+--
+-- Filtros para la tabla `patient_area`
+--
+ALTER TABLE `patient_area`
+  ADD CONSTRAINT `patient_area_ibfk_1` FOREIGN KEY (`patientid`) REFERENCES `patients` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `patient_area_ibfk_2` FOREIGN KEY (`areaid`) REFERENCES `areas` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
--- templates/users/users.tpl
+++ templates/users/users.tpl
@@ -1,32 +1,27 @@
 {include "../header.tpl"}
 
 <body>
-    {if $admin}
-        <a href="user-add">Agregar usuario</a>
-    {/if}
+    <a href="user-add">Agregar usuario</a>
+
 
     {if $users}
         {foreach from=$users item=user }
             <div>
                 <p>{$user->name} {$user->lastname}</p>
-                <form action="user" method="GET">
+    
+                <form action="user-edit" method="GET">
                     <input type="hidden" name="id" value="{$user->id}">
-                    <button>Ver</button>
+                    <button>Editar</button>
                 </form>
-                {if $admin}
-                    <form action="user-edit" method="GET">
-                        <input type="hidden" name="id" value="{$user->id}">
-                        <button>Editar</button>
-                    </form>
-                    <form action="db-user-delete" method="POST" onsubmit="return window.confirm('¿Seguro que desea eliminar el usuario?')">
-                        <input type="hidden" name="id" value="{$user->id}">
-                        <button>Eliminar</button>
-                    </form>
-                {/if}
+                <form action="db-user-delete" method="POST" onsubmit="return window.confirm('¿Seguro que desea eliminar el usuario?')">
+                    <input type="hidden" name="id" value="{$user->id}">
+                    <button>Eliminar</button>
+                </form>
+
             </div>
         {/foreach}
     {else}
-        <h3>Aun no tenemos usuarios registrados.</h3>
+        <h3>Aun no hay usuarios registrados.</h3>
 
     {/if}
 </body>
